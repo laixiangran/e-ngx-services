bc650893c795cb99dfb437058eba3fbef750b6ab
laixiangran <laixiangran@163.com>
2018-02-06 12:55:48 +0800

    服务全部导出

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,17 @@
  * homepage：http://www.laixiangran.cn.
  */
 import {ENgxServicesModule} from './e-ngx-services.module';
+import {GeolocationService} from './services/geolocation.service';
+import {AMapWebService} from './services/amap-web.service';
+import {EventsService} from './services/events.service';
+import {FilePathService} from './services/filepath.service';
+import {TransformService} from './services/transform.service';
+import {DateTimeService} from './services/datetime.service';
 
 export { ENgxServicesModule } from './e-ngx-services.module'
+export { AMapWebService } from './services/amap-web.service'
+export { GeolocationService } from './services/geolocation.service'
+export { EventsService } from './services/events.service'
+export { FilePathService } from './services/filepath.service'
+export { TransformService } from './services/transform.service'
+export { DateTimeService } from './services/datetime.service'
